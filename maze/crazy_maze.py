d0b42c2cf766ff70b3bffe995e2f4deae1a78d3a
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-01-20 04:20:34 +0200

    finalising maze creation algo

--- maze/crazy_maze.py
+++ maze/crazy_maze.py
@@ -1,12 +1,12 @@
 import turtle
 import random
 
-# min_x, min_y, max_x, max_y = random.choice([(
-#     -100, -100, 100, 100), (-200, -100, 200, 100), (-100, -200, 100, 200)])
-min_x, min_y, max_x, max_y = -100, -100, 100, 100
-# print("box Size", (min_x, min_y, max_x, max_y))
-# cell_size = random.choice([10, 20, 25,])
-cell_size = random.choice([50])
+min_x, min_y, max_x, max_y = random.choice([(
+    -100, -100, 100, 100), (-200, -100, 200, 100), (-100, -200, 100, 200), (-500, -300, 500, 300)])
+# min_x, min_y, max_x, max_y = -100, -100, 100, 100
+print("box Size", (min_x, min_y, max_x, max_y))
+cell_size = random.choice([10, 20, 25,])
+# cell_size = random.choice([50])
 
 # print("Cell size", cell_size)
 
@@ -32,7 +32,7 @@ def draw_square(cell_size, turtle_name, color="black", pen_color="red"):
     turtle_name.begin_poly()
     turtle_name.pencolor(pen_color)
     turtle_name.fillcolor(color)
-    turtle_name.pen(shown=True, pencolor=pen_color, fillcolor=color, speed=1)
+    turtle_name.pen(shown=True, pencolor=pen_color, fillcolor=color, speed=0)
     turtle_name.begin_fill()
     for _ in range(4):
         turtle_name.forward(cell_size)
@@ -43,14 +43,14 @@ def draw_square(cell_size, turtle_name, color="black", pen_color="red"):
     return turtle_name.get_poly()
     pass
 
-
+# TODO: Clean up this function by removing code that is not being used anymore and write comments
 def fill_in_constraints_box(cell_size, min_x, max_y, vertical_cells, horizontal_cells):
     list_of_blocks = []
     list_of_all_boxes_1d = []
     print("Vertical", vertical_cells)
     print("Horizontal", horizontal_cells)
     t = turtle.Turtle()
-    t.speed(1)
+    t.speed(0)
     t.getscreen().tracer(0)
     t.penup()
     t.goto(min_x, max_y)
@@ -73,18 +73,17 @@ def fill_in_constraints_box(cell_size, min_x, max_y, vertical_cells, horizontal_
 
     print(list_of_all_boxes_1d)
     print(len(list_of_all_boxes_1d))
-    # [print(index, " -", box) for index, box in enumerate(list_of_blocks)]
-    # print(len(list_of_blocks)*len(list_of_blocks[0]))
-    # [print(block) for block in list_of_blocks]
+
     return list_of_blocks, list_of_all_boxes_1d
 
     pass
 
 
-def choose_random_move_index(current_index, x_cells, y_cells, visited_list):
+def choose_random_move_index(current_index, x_cells, y_cells, visited_list, stack, maze_route):
     # print(current_index + x_cells)
     len_of_obs = (x_cells*y_cells)
-
+    if current_index == None:
+        current_index = 0
     # Check the first index, and choose an index to place the wall.
     if current_index == 0:
         down = current_index + x_cells
@@ -148,6 +147,13 @@ def choose_random_move_index(current_index, x_cells, y_cells, visited_list):
         right = current_index+1
         list_of_directions = [up, down, left, right]
 
+    in_visited = []
+    for direction in list_of_directions:
+        if direction in visited_list:
+            in_visited.append(True)
+        elif direction < len_of_obs:
+            in_visited.append(False)
+            
     while True:
         random_index = random.choice(list_of_directions)
         if random_index not in visited_list:
@@ -160,13 +166,25 @@ def choose_random_move_index(current_index, x_cells, y_cells, visited_list):
                 if len(list_of_directions) == 0:
                     wall_index = None
                     break
-
-            # indexes.append(random_index)
-            # indexes.append(wall_index)
             break
         else:
-            pass
-
+            if all(in_visited) and len(stack) != 1:
+                # Do the stack reverse here
+                print("going back")
+                stack.pop()
+                random_index = stack[-1]
+                wall_index = None
+                break
+            elif len(stack) == 1:
+                random_index = None
+                for i in range(0, len_of_obs):
+                    if i not in visited_list:
+                        wall_index = i
+                        break   
+                if wall_index:
+                    break
+                  
+        
     return random_index, wall_index
 
 
@@ -186,13 +204,13 @@ def create_maze_route(cell_size, min_x, min_y, max_x, max_y, list_of_blocks, lis
     maze_route = [0]
     maze_wall_list = []
     visited_list = [0]
-    stack = []
+    stack = [0]
 
     route = turtle.Turtle()
-    route.speed(1)
+    route.speed(0)
 
     wall = turtle.Turtle()
-    wall.speed(1)
+    wall.speed(0)
     route.pen(pencolor="green", pendown=False, fillcolor="white")
     wall.penup()
     wall.goto(min_x, max_y)
@@ -207,27 +225,27 @@ def create_maze_route(cell_size, min_x, min_y, max_x, max_y, list_of_blocks, lis
     while len(visited_list) < (len(list_of_all_boxes_1d)):
 
         random_index, wall_index = choose_random_move_index(
-            current_index, horizontal_cells, vertical_cells, visited_list)
-
-        # if random_index == len(list_of_all_boxes_1d):
-        #     current_index == random_index-1
-        # else: current_index = random_index
-        # if wall_index == len(list_of_all_boxes_1d):
-        #     current_wall_index = wall_index-1
-        # else:
-        # current_wall_index = wall_index
+            current_index, horizontal_cells, vertical_cells, visited_list, stack, maze_route)
 
         current_index = random_index
         current_wall_index = wall_index
 
-        if current_index not in visited_list and current_wall_index not in visited_list or current_wall_index == None and current_index < len(list_of_all_boxes_1d):
+        if current_index not in visited_list or current_index == None and current_wall_index not in visited_list or current_wall_index == None and current_index < len(list_of_all_boxes_1d):
+            # check if the  there are no more places to go
+            if len(stack) != 0:
+                pass
+            
             if current_index != None and current_index < len(list_of_all_boxes_1d):
-                visited_list.append(current_index)
+                if current_index not  in visited_list:
+                    visited_list.append(current_index)
+                if current_index not in stack:
+                    stack.append(current_index)
                 maze_route.append(current_index)
                 route.goto(list_of_all_boxes_1d[current_index][0])
                 draw_square(cell_size, route, "green", "black")
             if current_wall_index != None and current_wall_index < len(list_of_all_boxes_1d):
                 visited_list.append(current_wall_index)
+                # stack.append(current_wall_index)
                 maze_wall_list.append(current_wall_index)
                 wall.goto(list_of_all_boxes_1d[current_wall_index][0])
                 draw_square(cell_size, wall, "red", "black")
@@ -237,118 +255,6 @@ def create_maze_route(cell_size, min_x, min_y, max_x, max_y, list_of_blocks, lis
     print("Path List: ", maze_route)
     print()
     print("Visited List: ", sorted(visited_list))
-
-    # if current_index == len(list_of_all_boxes_1d):
-    #     break
-
-    # for line_of_blocks in list_of_blocks:
-    #     current_line_index = list_of_blocks.index(line_of_blocks)
-    #     """Get a random line index and Check to make sure that the position is valid."""
-    #     if current_line_index == 0:
-    #         rand_line = random.randint(
-    #             current_line_index, current_line_index+1)
-    #         if rand_line == current_line_index:
-    #             wall_line = current_line_index+1
-    #         else:
-    #             wall_line = current_line_index
-    #     elif current_line_index == (vertical_cells-1):
-    #         rand_line = random.randint(
-    #             current_line_index-1, current_line_index)
-    #         if rand_line == current_line_index:
-    #             wall_line = current_line_index-1
-    #         else:
-    #             wall_line = current_line_index
-    #     else:
-    #         rand_line = random.randint(
-    #             current_line_index-1, current_line_index+1)
-    #         # FIXED: LOGIC NEEDS to be fixed
-    #         if rand_line == current_line_index-1:
-    #             # TODO: Add an if statement to check if the block hasn't been used already.
-    #             if current_line_index+1 in visited_list:
-    #                 wall_line = current_line_index+1
-    #             else:
-    #                 wall_line = current_line_index
-
-    #         elif rand_line == current_line_index+1:
-    #             if current_line_index-1 in visited_list:
-    #                 wall_line = current_line_index-1
-    #             else:
-    #                 wall_line = current_line_index
-    #         else:
-    #             wall_line = current_line_index
-
-    #     # print(rand_line)
-
-    #     for block in line_of_blocks:
-    #         current_block_index = line_of_blocks.index(block)
-
-    #         """Generate random block index and Check to make sure that the position is valid."""
-    #         if current_block_index == 0:
-    #             rand_block = random.randint(
-    #                 current_block_index, current_block_index+1)
-    #             if rand_block == current_block_index:
-    #                 wall_line = current_block_index+1
-    #             else:
-    #                 wall_line = current_block_index
-    #         elif current_block_index == horizontal_cells-1:
-    #             rand_block = random.randint(
-    #                 current_block_index-1, current_block_index)
-    #             if rand_block == current_block_index:
-    #                 wall_line = current_block_index-1
-    #             else:
-    #                 wall_line = current_block_index
-    #         else:
-    #             rand_block = random.randint(
-    #                 current_block_index-1, current_block_index+1)
-    #             if rand_line == current_block_index-1:
-    #             # TODO: Add an if statement to check if the block hasn't been used already.
-    #                 if current_block_index+1 in visited_list:
-    #                     wall_line = current_block_index+1
-    #                 else:
-    #                     wall_line = current_block_index
-
-    #             elif rand_line == current_block_index+1:
-    #                 if current_block_index-1 in visited_list:
-    #                     wall_line = current_block_index-1
-    #                 else:
-    #                     wall_line = current_block_index
-    #             else:
-    #                 wall_line = current_block_index
-
-    #         if current_line_index != rand_line:
-    #             """Move the Y(line) axis only"""
-    #             print("Moving The Line")
-    #             path_pos = list_of_blocks[rand_line][current_block_index]
-    #             obs_pos = list_of_blocks[wall_line][current_block_index]
-    #         else:
-    #             """Move the block in the (X) axis"""
-    #             print("Moving The Block")
-    #             path_pos = list_of_blocks[current_line_index][rand_block]
-    #             obs_pos = list_of_blocks[wall_line][rand_block]
-
-    #         if path_pos not in visited_list and path_pos not in maze_wall_list:
-    #             route.penup()
-    #             route.goto(path_pos[0])
-    #             route.pendown()
-    #             draw_square(cell_size, route, "white", "black")
-    #             if obs_pos in visited_list or obs_pos in maze_wall_list:
-    #                 continue
-    #             else:
-    #                 wall.goto(obs_pos[0])
-    #                 wall.pendown()
-    #                 draw_square(cell_size, wall, "blue", "red")
-    #                 wall.penup()
-    #                 maze_wall_list.append(obs_pos)
-    #                 visited_list.append(obs_pos)
-    #                 print("Drawing Wall")
-
-    #             print("Drawing Path")
-    #             visited_list.append(path_pos)
-
-    #             # list_of_blocks[rand_line].pop(current_block_index)
-    #         else:
-    #             print("Already Used")
-    #             continue
 
 
 def check_right():
